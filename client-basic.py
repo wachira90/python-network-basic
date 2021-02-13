665d98b9fc316503c324c97f01053610ea4534ee
Wachira Duangdee <wcrcorp@gmail.com>
2021-02-13 17:30:08 +0700

    Add files via upload

---
+++ client-basic.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+print("send data to port ", PORT)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
